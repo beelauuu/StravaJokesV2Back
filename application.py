29ea11eaa511c8c850aa0593217361c988f5a114
beelauuu <103338659+beelauuu@users.noreply.github.com>
2023-06-19 00:08:45 -0600

    added in webhook and modified script

--- application.py
+++ application.py
@@ -3,103 +3,125 @@ import os
 from flask import Flask, redirect, request
 from flask import jsonify
 from dotenv import load_dotenv
+from pymongo import MongoClient
 import requests
 
+# Setting up the flask app, database, and environment variables
 app = Flask(__name__)
 load_dotenv()
-client_id=os.environ.get('CLIENT_ID'),
-client_secret=os.environ.get('CLIENT_SECRET'),
-refresh_token=os.environ.get('REFRESH_TOKEN')
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+client = MongoClient(
+  f'mongodb+srv://{os.environ.get("DB_USERNAME")}:{os.environ.get("DB_PASSWORD")}@cluster0.ziyuaoy.mongodb.net/?retryWrites=true&w=majority'
+)
+db = client['StravaJokes']
+collection = db['users']
 
+# Standard Start Route
 @app.route('/')
 def api_root():
-    return 'This means it is working :)'
+  return 'Welcome'
 
+# Login Route (redirects to Strava's auth)
 @app.route('/login')
 def login():
-    client_id = os.environ.get('CLIENT_ID')
-    redirect_uri = 'http://127.0.0.1:5000/callback'
-    scopes = 'activity:write,activity:read_all'  # Adjust the scopes according to your requirements
-    authorization_url = f'https://www.strava.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scopes}'
-    return redirect(authorization_url)
+  client_id = os.environ.get('CLIENT_ID')
+  redirect_uri = 'https://stravajokesv2.beelauuu.repl.co/callback'
+  scopes = 'activity:write,activity:read_all'
+  authorization_url = f'https://www.strava.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scopes}'
+  return redirect(authorization_url)
 
-import requests
-from flask import redirect, jsonify, request
-
+# Callback route after authenticating
 @app.route('/callback')
 def strava_callback():
-    code = request.args.get('code')
-    # Exchange the authorization code for an access token
-    token_url = 'https://www.strava.com/oauth/token'
-    payload = {
-        'client_id': os.environ.get('CLIENT_ID'),
-        'client_secret': os.environ.get('CLIENT_SECRET'),
-        'code': code,
-        'grant_type': 'authorization_code'
+  code = request.args.get('code')
+  # Exchange the authorization code for an access token
+  token_url = 'https://www.strava.com/oauth/token'
+  payload = {
+    'client_id': os.environ.get('CLIENT_ID'),
+    'client_secret': os.environ.get('CLIENT_SECRET'),
+    'code': code,
+    'grant_type': 'authorization_code'
+  }
+  # Parsing and storing token from response 
+  response = requests.post(token_url, data=payload)
+  data = response.json()
+  # Checking for a valid response   
+  if 'access_token' in data:
+    access_token = data['access_token']
+    refresh_token = data['refresh_token']
+    user_id = data['athlete']['id']
+    existing_user = collection.find_one({'user_id': user_id})
+
+    # Store tokens in MongoDB (if user DNE)
+    if existing_user == None:
+      user_tokens = {
+        'user_id': user_id,
+        'access_token': access_token,
+        'refresh_token': refresh_token
+      }
+      collection.insert_one(user_tokens)
+
+    # Create a POST subscription for the webhook
+    subscription_url = 'https://www.strava.com/api/v3/push_subscriptions'
+    subscription_payload = {
+      'client_id': os.environ.get('CLIENT_ID'),
+      'client_secret': os.environ.get('CLIENT_SECRET'),
+      'callback_url': 'https://stravajokesv2.beelauuu.repl.co/webhook',
+      'verify_token': 'BEELAU'
     }
-    response = requests.post(token_url, data=payload)
-    data = response.json()
+    headers = {'Authorization': f'Bearer {access_token}'}
+    subscription_response = requests.post(subscription_url,
+                                          data=subscription_payload,
+                                          headers=headers)
 
-    if 'access_token' in data:
-        access_token = data['access_token']
-        refresh_token = data['refresh_token']
-        
-        # Store the access_token and refresh_token securely and associate them with the authenticated user
-        # This step depends on your application's data storage and user management system
-        
-        # Create a push subscription for the authenticated user
-        subscription_url = 'https://www.strava.com/api/v3/push_subscriptions'
-        subscription_payload = {
-            'client_id': os.environ.get('CLIENT_ID'),
-            'client_secret': os.environ.get('CLIENT_SECRET'),
-            'callback_url': 'http://127.0.0.1:5000/webhook',
-            'verify_token': 'BEELAU'
-        }
-        headers = {
-            'Authorization': f'Bearer {access_token}'
-        }
-        subscription_response = requests.post(subscription_url, data=subscription_payload, headers=headers)
-        print(subscription_response.json())
-        # Check the response status code to ensure the subscription was created successfully
-        if subscription_response.status_code == 201:
-            # Subscription created successfully
-            return jsonify({'message': 'Subscription created'}), 200
-        else:
-            # Failed to create subscription
-            return jsonify({'message': 'Failed to create subscription'}), 500
+    # Check the response status code to ensure the subscription was created successfully
+    if subscription_response.status_code == 201:
+      # Subscription created successfully
+      return jsonify({'message': 'Subscription created'}), 200
     else:
-        # Failed to obtain access token
-        return jsonify({'message': 'Failed to obtain access token'}), 500
-
+      # Failed to create subscription
+      return jsonify({'message': 'Failed to create subscription. Most likely you are already subscribed'}), 500
+  else:
+    # Failed to obtain access token
+    return jsonify({'message': 'Failed to obtain access token'}), 500
 
 
 # Creates the endpoint for our webhook
 @app.route('/webhook', methods=['POST'])
-def webhook():
-    print("Webhook event received!", request.args, request.json)
-    return 'EVENT_RECEIVED', 200
+async def webhook():
+  #Catching the webhook events & information  
+  print("Webhook event received!", request.args, request.json)
+  #If an activity is created, run the joke update function  
+  if request.json['aspect_type'] == 'create' and request.json[
+      'object_type'] == 'activity':
+    await joke.update_joke(request.json['owner_id'])
+    return 'JOKE_RECEIVED', 200
+  return 'EVENT_RECEIVED', 200
+
 
 # Adds support for GET requests to our webhook
 @app.route('/webhook', methods=['GET'])
 def verify_webhook():
-    # Your verify token. Should be a random string.
-    VERIFY_TOKEN = "BEELAU"
-    # Parses the query params
-    mode = request.args.get('hub.mode')
-    token = request.args.get('hub.verify_token')
-    challenge = request.args.get('hub.challenge')
+  # Your verify token. Should be a random string.
+  VERIFY_TOKEN = "BEELAU"
+  # Parses the query params
+  mode = request.args.get('hub.mode')
+  token = request.args.get('hub.verify_token')
+  challenge = request.args.get('hub.challenge')
 
-    # Checks if a token and mode is in the query string of the request
-    if mode and token:
-        # Verifies that the mode and token sent are valid
-        if mode == 'subscribe' and token == VERIFY_TOKEN:
-            # Responds with the challenge token from the request
-            print('WEBHOOK_VERIFIED')
-            return jsonify({'hub.challenge': challenge}), 200
-        else:
-            # Responds with '403 Forbidden' if verify tokens do not match
-            return 'Forbidden', 403
-    return 'Bad Request', 400
-        
+  # Checks if a token and mode is in the query string of the request
+  if mode and token:
+    # Verifies that the mode and token sent are valid
+    if mode == 'subscribe' and token == VERIFY_TOKEN:
+      # Responds with the challenge token from the request
+      print('WEBHOOK_VERIFIED')
+      return jsonify({'hub.challenge': challenge}), 200
+    else:
+      # Responds with '403 Forbidden' if verify tokens do not match
+      return 'Forbidden', 403
+  return 'Bad Request', 400
+
+# For some reason, need to include the 0.0.0.0 for it to work.
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run('0.0.0.0')
--- joke.py
+++ joke.py
@@ -1,89 +1,80 @@
 import requests
 from dotenv import load_dotenv
 import os
-from jokeapi import Jokes 
+from jokeapi import Jokes
+from pymongo import MongoClient
 import asyncio
 
 auth_url = 'https://www.strava.com/api/v3/oauth/token'
 url = 'https://www.strava.com/api/v3'
 load_dotenv()
+client = MongoClient(
+  'mongodb+srv://blau1:SwPJFDVOvQ8aLAUm@cluster0.ziyuaoy.mongodb.net/?retryWrites=true&w=majority'
+)
+db = client['StravaJokes']
+collection = db['users']
 
-#For initial authorization
-# payload = {
-#     'client_id' : os.environ.get('CLIENT_ID')
-#     'client_secret': os.environ.get('CLIENT_SECRET'),
-#     'grant_type':'authorization_code',
-#     'code': os.environ.get('CODE')
-# }
+async def update_joke(user_id):
+  #Get the users tokens, if they exist   
+  user = collection.find_one({'user_id': user_id})
+  if user is None:
+    return 'FAILED'
+  
+  #Retrieving refresh token
+  payload = {
+    'client_id': os.environ.get('CLIENT_ID'),
+    'client_secret': os.environ.get('CLIENT_SECRET'),
+    'grant_type': 'refresh_token',
+    'refresh_token': user['refresh_token']
+  }
+  response = requests.post(
+    auth_url,
+    data=payload,
+  )
+  access_token = response.json()['access_token']
 
-async def update_joke(): 
-    #Retrieving refresh token
-    payload = {
-        'client_id' : os.environ.get('CLIENT_ID'),
-        'client_secret': os.environ.get('CLIENT_SECRET'),
-        'grant_type':'refresh_token',
-        'refresh_token': os.environ.get('REFRESH_TOKEN')
-    }
-    response = requests.post(
-        auth_url,
-        data=payload,
-    )
-    access_token = response.json()['access_token']
+  #Getting first (most recent) activity id
+  headers = {'Authorization': 'Bearer ' + access_token}
+  param = {'page': 1, 'per_page': 1}
+  response = requests.get(url + '/athlete/activities',
+                          headers=headers,
+                          params=param)
+  activity_id = response.json()[0]['id']
 
-    #Getting first (most recent) activity id
-    headers= {
-        'Authorization': 'Bearer ' + access_token 
-    }
-    param = {
-    'page': 1,
-    'per_page': 1
-    }
-    response = requests.get(
-        url+'/athlete/activities',
-        headers=headers,
-        params=param
-    )
-    activity_id = response.json()[0]['id']
+  #Get current description
+  headers = {'Authorization': 'Bearer ' + access_token}
+  response = requests.get(
+    url + '/activities/' + str(activity_id),
+    headers=headers,
+  )
+  current_description = response.json()['description']
+  if current_description is None:
+    current_description = ''
 
-    #Get current description
-    headers= {
-        'Authorization': 'Bearer ' + access_token 
-    }
-    response = requests.get(
-        url+'/activities/'+str(activity_id),
-        headers=headers,
-    )
-    current_description = response.json()['description']
-    if current_description is None:
-        current_description = ''
-
-    #Updating activity description
-    if('🤡 Joke of the day 🤡' not in current_description):
-        j = await Jokes()  # Initialise the class
-        joke = await j.get_joke(blacklist=['sexist', 'racist'])  # Retrieve a random joke
-        # One-liner joke
-        if joke["type"] == "single": 
-            headers= {
-            'Authorization': 'Bearer ' + access_token 
-            }
-            updatableActivity = {
-            'description': '🤡 Joke of the day 🤡\n' + joke["joke"] + '\n- by Joke.py v2' + '\n\n' + current_description
-            }
-            response = requests.put(
-            url+'/activities/'+str(activity_id),
-            headers=headers,
-            params=updatableActivity
-            )
-        # Two-part joke
-        else:
-            headers= {
-            'Authorization': 'Bearer ' + access_token 
-            }
-            updatableActivity = {
-            'description': '🤡 Joke of the day 🤡\n' + joke["setup"] + '\n' + joke["delivery"] + '\n- by Joke.py v2' + '\n\n' + current_description
-            }
-            response = requests.put(
-            url+'/activities/'+str(activity_id),
-            headers=headers,
-            params=updatableActivity
-            )
+  #Updating activity description
+  if ('🤡 Joke of the day 🤡' not in current_description):
+    j = await Jokes()  # Initialise the class
+    joke = await j.get_joke(blacklist=['explicit','sexist', 'racist'])  # Retrieve a random joke
+    
+    # One-liner joke
+    if joke["type"] == "single":
+      headers = {'Authorization': 'Bearer ' + access_token}
+      updatableActivity = {
+        'description':
+        '🤡 Joke of the day 🤡\n' + joke["joke"] + '\n- by Joke.py v2' + '\n\n' +
+        current_description
+      }
+      response = requests.put(url + '/activities/' + str(activity_id),
+                              headers=headers,
+                              params=updatableActivity)
+    # Two-part joke
+    else:
+      headers = {'Authorization': 'Bearer ' + access_token}
+      updatableActivity = {
+        'description':
+        '🤡 Joke of the day 🤡\n' + joke["setup"] + '\n' + joke["delivery"] +
+        '\n- by Joke.py (v2)' + '\n\n' + current_description
+      }
+      response = requests.put(url + '/activities/' + str(activity_id),
+                              headers=headers,
+                              params=updatableActivity)
